b1bf48363ce9d2d8eb96737320e270967899e009
SRarunachalam <142921688+SRarunachalam@users.noreply.github.com>
2023-08-22 16:28:40 +0000

    oddoreven completed

---
+++ oddoreven.java
@@ -0,0 +1,10 @@
+class oddoreven{
+    public static void main(String[]args){
+        int num=10;
+        if(num%2!=0)
+        System.out.println("the given number is odd");
+        else
+
+System.out.println("the given number is even");
+    }
+}
