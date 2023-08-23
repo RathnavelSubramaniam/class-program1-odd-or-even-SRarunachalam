d880f3ab2d4f85eb3225527fc74c616ca5d23f55
SRarunachalam <142921688+SRarunachalam@users.noreply.github.com>
2023-08-23 11:01:26 +0530

    Update oddoreven.java

--- oddoreven.java
+++ oddoreven.java
@@ -1,7 +1,7 @@
 class oddoreven{
     public static void main(String[]args){
-        int num=10;
-        if(num%2!=0)
+        int num=3;
+        if(num%2==0)
         System.out.println("the given number is odd");
         else
 
