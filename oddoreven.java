672a1d5ea922952e511258a2e79a80499851b9f3
SRarunachalam <142921688+SRarunachalam@users.noreply.github.com>
2023-08-31 05:21:06 +0000

    comp

--- oddoreven.java
+++ oddoreven.java
@@ -1,9 +1,17 @@
 class oddoreven{
     public static void main(String args[]){
-        int num=3;
+        int num1=3;
+        if (num1%2!=0)
+        System.out.println("The given number is ODD");
+        else
+         System.out.println("The given number is EVEN");
+
+         int num=10;
         if (num%2!=0)
         System.out.println("The given number is ODD");
         else
          System.out.println("The given number is EVEN");
+    
+
     }
 }
